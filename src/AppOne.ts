93d349dac6068bfdb1c52535c8b808dd593d9db0
Itskatt <raph10@mail.com>
2024-02-06 17:35:00 +0100

    Prise en charge basique des controles du joueur

--- src/AppOne.ts
+++ src/AppOne.ts
@@ -1,15 +1,22 @@
-import * as BABYLON from "babylonjs";
+import { ArcRotateCamera, Color3, Engine, HemisphericLight, MeshBuilder, Scene, StandardMaterial, Vector3 } from "babylonjs";
+import { PlayerInput } from "./PLayerInput";
 
 export class AppOne {
-    engine: BABYLON.Engine;
-    scene: BABYLON.Scene;
+    private engine: Engine;
+    private scene: Scene;
+
+    private input: PlayerInput;
 
     constructor(readonly canvas: HTMLCanvasElement) {
-        this.engine = new BABYLON.Engine(canvas);
+        this.engine = new Engine(canvas);
         window.addEventListener("resize", () => {
             this.engine.resize();
         });
+
         this.scene = createScene(this.engine, this.canvas);
+
+        // Mouvements (tests)
+        this.input = new PlayerInput(this.scene);
     }
 
     debug(debugOn: boolean = true) {
@@ -28,17 +35,17 @@ export class AppOne {
     }
 }
 
-const createScene = (engine: BABYLON.Engine, canvas: HTMLCanvasElement) => {
+const createScene = (engine: Engine, canvas: HTMLCanvasElement) => {
     // This creates a basic Babylon Scene object (non-mesh)
-    const scene = new BABYLON.Scene(engine);
+    const scene = new Scene(engine);
 
     // La camera
-    const camera = new BABYLON.ArcRotateCamera(
+    const camera = new ArcRotateCamera(
         "camera",
         0,
         1,
         15,
-        BABYLON.Vector3.Zero(),
+        Vector3.Zero(),
         scene
     );
 
@@ -46,58 +53,61 @@ const createScene = (engine: BABYLON.Engine, canvas: HTMLCanvasElement) => {
     camera.attachControl(canvas, true);
 
     // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    const light = new BABYLON.HemisphericLight(
+    const light = new HemisphericLight(
         "light",
-        new BABYLON.Vector3(0, 1, 0),
+        new Vector3(0, 1, 0),
         scene
     );
 
     light.intensity = 0.7;
 
     // Le sol
-    const ground = BABYLON.MeshBuilder.CreateGround(
+    const ground = MeshBuilder.CreateGround(
         "ground",
         { width: 5, height: 15 },
         scene
     );
-    const groundMaterial = new BABYLON.StandardMaterial(
+    const groundMaterial = new StandardMaterial(
         "groundMaterial",
         scene
     );
-    groundMaterial.diffuseColor = new BABYLON.Color3(0.5, 0.8, 0.5);
+    groundMaterial.diffuseColor = new Color3(0.5, 0.8, 0.5);
     ground.material = groundMaterial;
 
     // Le filet
-    const frontPole = BABYLON.MeshBuilder.CreateCylinder(
+    const frontPole = MeshBuilder.CreateCylinder(
         "front-pole",
         { height: 2, diameter: 0.4 },
         scene
-    )
+    );
     frontPole.position.x = 2;
     frontPole.position.y = 1;
 
-    const backPole = BABYLON.MeshBuilder.CreateCylinder(
+    const backPole = MeshBuilder.CreateCylinder(
         "back-pole",
         { height: 2, diameter: 0.4 },
         scene
-    )
+    );
     backPole.position.x = -2;
     backPole.position.y = 1;
 
     frontPole.parent = ground;
     backPole.parent = ground;
 
-    const wall = BABYLON.MeshBuilder.CreateBox(
-        "wall",
-        {
-            width: 5,
-            depth: 0.5,
-            height: 2,
-        }
-    )
-    wall.position.y = 1
+    // Le mur
+    const wall = MeshBuilder.CreateBox("wall", {
+        width: 5,
+        depth: 0.5,
+        height: 2,
+    });
+    wall.position.y = 1;
 
-    wall.parent = ground
+    wall.parent = ground;
+
+    // Un "joueur"
+    const leftPlayer = MeshBuilder.CreateCylinder("left-player");
+    leftPlayer.position.y = 1;
+    leftPlayer.position.z = -3.5;
 
     scene.registerBeforeRender(() => {});
 
---
+++ src/PlayerInput.ts
@@ -0,0 +1,68 @@
+import { ActionManager, ExecuteCodeAction, Scalar, Scene } from "babylonjs";
+
+// https://doc.babylonjs.com/guidedLearning/createAGame/characterMovePt1
+export class PlayerInput {
+    private inputMap = new Map();
+
+    private horizontal = 0;
+    private horizontalAxis = 0;
+
+    constructor(scene: Scene) {
+        scene.actionManager = new ActionManager(scene);
+
+        scene.actionManager.registerAction(
+            new ExecuteCodeAction(ActionManager.OnKeyDownTrigger, (evt) => {
+                this.inputMap.set(
+                    evt.sourceEvent.key,
+                    evt.sourceEvent.type == "keydown"
+                );
+            })
+        );
+        scene.actionManager.registerAction(
+            new ExecuteCodeAction(ActionManager.OnKeyUpTrigger, (evt) => {
+                this.inputMap.set(
+                    evt.sourceEvent.key,
+                    evt.sourceEvent.type == "keydown"
+                );
+            })
+        );
+
+        scene.onBeforeRenderObservable.add(() => {
+            // console.log(this.inputMap);
+            
+            this.updateFromKeyboard()
+        });
+    }
+
+    private updateFromKeyboard(): void {
+        /// Mouvement vertical
+        if (this.inputMap.get("z")) {
+            // Sauter
+            console.log("Je saute");
+
+        } else if (this.inputMap.get("s")) {
+            // Retour au sol
+            console.log("Je descend !");
+
+        } else {
+            // Plus de mouvement vertical
+        }
+
+        /// Mouvement horizontal
+        if (this.inputMap.get("q")) {
+            // Gauche
+            this.horizontal = Scalar.Lerp(this.horizontal, -1, 0.2);
+            this.horizontalAxis = -1;
+
+        } else if (this.inputMap.get("d")) {
+            // Droite
+            this.horizontal = Scalar.Lerp(this.horizontal, 1, 0.2);
+            this.horizontalAxis = 1;
+
+        } else {
+            // Plus de mouvement horizontal
+            this.horizontal = 0;
+            this.horizontalAxis = 0;
+        }
+    }
+}
