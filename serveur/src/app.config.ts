301ca2e51ef4ee7a345a914921582093c7ba9981
VEILLARD corentin <corentin.veillard@etu.univ-cotedazur.fr>
2024-02-07 17:34:28 +0100

    colyseus examples

---
+++ serveur/src/app.config.ts
@@ -0,0 +1,85 @@
+import config from "@colyseus/tools";
+import { monitor } from "@colyseus/monitor";
+import { playground } from "@colyseus/playground";
+import { auth } from "@colyseus/auth";
+import path from 'path';
+import serveIndex from 'serve-index';
+import express from 'express';
+
+// import { uWebSocketsTransport} from "@colyseus/uwebsockets-transport";
+import "./config/auth";
+
+// Import demo room handlers
+import { LobbyRoom, RelayRoom } from 'colyseus';
+import { ChatRoom } from "./rooms/01-chat-room";
+import { StateHandlerRoom } from "./rooms/02-state-handler";
+import { AuthRoom } from "./rooms/03-auth";
+import { ReconnectionRoom } from './rooms/04-reconnection';
+import { CustomLobbyRoom } from './rooms/07-custom-lobby-room';
+
+export default config({
+    options: {
+        devMode: true,
+    },
+
+    initializeGameServer: (gameServer) => {
+        // Define "lobby" room
+        gameServer.define("lobby", LobbyRoom);
+
+        // Define "relay" room
+        gameServer.define("relay", RelayRoom, { maxClients: 4 })
+            .enableRealtimeListing();
+
+        // Define "chat" room
+        gameServer.define("chat", ChatRoom)
+            .enableRealtimeListing();
+
+        // Register ChatRoom with initial options, as "chat_with_options"
+        // onInit(options) will receive client join options + options registered here.
+        gameServer.define("chat_with_options", ChatRoom, {
+            custom_options: "you can use me on Room#onCreate"
+        });
+
+        // Define "state_handler" room
+        gameServer.define("state_handler", StateHandlerRoom)
+            .enableRealtimeListing();
+
+        // Define "auth" room
+        gameServer.define("auth", AuthRoom)
+            .enableRealtimeListing();
+
+        // Define "reconnection" room
+        gameServer.define("reconnection", ReconnectionRoom)
+            .enableRealtimeListing();
+
+        // Define "custom_lobby" room
+        gameServer.define("custom_lobby", CustomLobbyRoom);
+
+        gameServer.onShutdown(function(){
+            console.log(`game server is going down.`);
+        });
+
+
+    },
+
+    initializeExpress: (app) => {
+        // (optional) auth module
+        app.use(auth.prefix, auth.routes());
+
+        // (optional) client playground
+        app.use('/playground', playground);
+
+        // (optional) web monitoring panel
+        app.use('/colyseus', monitor());
+
+        app.use('/', serveIndex(path.join(__dirname, "static"), {'icons': true}))
+        app.use('/', express.static(path.join(__dirname, "static")));
+    },
+
+
+    beforeListen: () => {
+        /**
+         * Before before gameServer.listen() is called.
+         */
+    }
+});
