e343a2420b24180c88b1c1c35c2a9a36d0b5d0b9
Itskat <raphaelecrc2@gmail.com>
2024-03-08 11:18:43 +0100

    Support pour ecran de chargement

--- frontend/src/gui/App.vue
+++ frontend/src/gui/App.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
+import Overlay from "./Overlay.vue";
 import "./main.css"
 import { onMounted, ref } from 'vue';
+
+const loading = ref(true)
 
 const renderCanvas = ref<HTMLCanvasElement | null>(null)
 
 onMounted(async () => {
   if (renderCanvas.value) {
     const module = await import('../AppOne');
+
+    loading.value = false
 
     let App = module.AppOne;
     let app = new App(renderCanvas.value);
@@ -18,7 +23,12 @@ onMounted(async () => {
 <template>
   <!-- Overlay -->
   <div class="absolute z-10">
-    I'm an overlay
+    <!-- <Overlay /> -->
+  </div>
+
+  <!-- Loading screen -->
+  <div v-if="loading" class="absolute z-20">
+    Chargement en cours...
   </div>
 
   <!-- Game canvas -->
