2ccf02c02fe6fe49d385bd139cdaef971bb6f075
Itskatt <raph10@mail.com>
2024-05-31 22:56:45 +0200

    Corriger un warning de compilation

--- frontend/src/gui/VolumeSlider.vue
+++ frontend/src/gui/VolumeSlider.vue
@@ -33,7 +33,7 @@ onMounted(() => {
 
 <template>
   <div class="relative mb-6" style="position: fixed; bottom: 10px; right: 30px; width: 500px;">
-    <img src="../../public/assets/Speaker_Icon.svg" alt="Speaker_Icon" class="absolute left-0 bottom-0" style="width: 40px; height: 40px; left: -50px; bottom: -10px;">
+    <img src="/assets/Speaker_Icon.svg" alt="Speaker_Icon" class="absolute left-0 bottom-0" style="width: 40px; height: 40px; left: -50px; bottom: -10px;">
     <label for="labels-range-input" class="sr-only">Labels range</label>
     <input id="labels-range-input" type="range" :value="sliderValue" @input="handleSliderChange" min="100" max="1500" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer range-lg dark:bg-gray-700">
     <span class="text-sm text-gray-500 dark:text-white absolute start-0 -bottom-6">Min </span>
